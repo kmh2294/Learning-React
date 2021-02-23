509b451aff9cd305b4cd9709169890196b3f4ef5
kmh2294 <aud2294@naver.com>
2021-02-23 16:45:50 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+    {
+        id: 1,
+        image: "https://placeimg.com/64/64/1",
+        name: "홍길동",
+        birthday: "961222",
+        gender: "남자",
+        job: "대학생",
+    },
+    {
+        id: 2,
+        image: "https://placeimg.com/64/64/2",
+        name: "멍충이",
+        birthday: "961222",
+        gender: "남자",
+        job: "대학생",
+    },
+    {
+        id: 3,
+        image: "https://placeimg.com/64/64/3",
+        name: "똥멍충이",
+        birthday: "961222",
+        gender: "남자",
+        job: "대학생",
+    },
+];
 
 function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>시작ddddddd해보자고 </h2>
-    </div>
-  );
+    return (
+        <div>
+            {customers.map((c) => {
+                return (
+                    <Customer
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
