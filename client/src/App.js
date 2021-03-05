76584879d6e3d5d9ce278885df7e7a6b46f7f5f9
kmh2294 <aud2294@naver.com>
2021-03-05 15:19:15 +0900

    search function implementation

--- client/src/App.js
+++ client/src/App.js
@@ -101,12 +101,14 @@ class App extends Component {
         this.state = {
             customers: "",
             completed: 0,
+            searchKeyword: "",
         };
     }
     stateRefresh = () => {
         this.setState({
             customers: "",
             completed: 0,
+            searchKeyword: "",
         });
         this.callApi().then((res) => this.setState({ customers: res }));
     };
@@ -126,7 +128,33 @@ class App extends Component {
         const { completed } = this.state;
         this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
     };
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    };
+
     render() {
+        const filteredComponents = (data) => {
+            data = data.filter((c) => {
+                return c.name.indexOf(this.state.searchKeyword) > -1;
+            });
+            return data.map((c) => {
+                return (
+                    <Customer
+                        stateRefresh={this.stateRefresh}
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                    ></Customer>
+                );
+            });
+        };
         const { classes } = this.props;
         const cellList = [
             "번호",
@@ -167,6 +195,9 @@ class App extends Component {
                                     input: classes.inputInput,
                                 }}
                                 inputProps={{ "aria-label": "search" }}
+                                name="searchKeyword"
+                                value={this.state.searchKeyword}
+                                onChange={this.handleValueChange}
                             />
                         </div>
                     </Toolbar>
@@ -192,20 +223,7 @@ class App extends Component {
                         </TableHead>
                         <TableBody>
                             {this.state.customers ? (
-                                this.state.customers.map((c) => {
-                                    return (
-                                        <Customer
-                                            stateRefresh={this.stateRefresh}
-                                            key={c.id}
-                                            id={c.id}
-                                            image={c.image}
-                                            name={c.name}
-                                            birthday={c.birthday}
-                                            gender={c.gender}
-                                            job={c.job}
-                                        />
-                                    );
-                                })
+                                filteredComponents(this.state.customers)
                             ) : (
                                 <TableRow>
                                     <TableCell colSpan="6" align="center">
